3a456159b547583f744065e7362cc55021b529a8
Harsh <sample@gmail.com>
2024-01-26 23:18:36 +0530

    second commit

--- src/main/java/in/ineuron/test/MainApp.java
+++ src/main/java/in/ineuron/test/MainApp.java
@@ -14,10 +14,16 @@ public class MainApp {
 		System.out.println("*************BeanFactory container started************");
 		
 		WishMessageGenerator wmg = xmlBeanFactory.getBean("wmg", WishMessageGenerator.class);
-		System.out.println(wmg);
+		System.out.println("HashCode of wmg is " + wmg.hashCode());
 		
 		String result = wmg.generateMessage("sachin");
 		System.out.println(result + "\n");
+		
+		WishMessageGenerator wmg1 = xmlBeanFactory.getBean("wmg", WishMessageGenerator.class);
+		System.out.println("HashCode of wmg1 is " + wmg1.hashCode());
+		
+		String result1 = wmg1.generateMessage("Virat");
+		System.out.println(result1 + "\n");
 		
 		System.out.println("**********Container is closing***********");
 	}
