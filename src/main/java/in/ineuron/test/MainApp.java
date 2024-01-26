15e03472d41b4449e115b7f4d9d1382a0dd4af6f
Harsh <sample@gmail.com>
2024-01-26 23:57:44 +0530

    first commit

--- src/main/java/in/ineuron/test/MainApp.java
+++ src/main/java/in/ineuron/test/MainApp.java
@@ -1,30 +1,28 @@
 package in.ineuron.test;
 
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
-
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import in.ineuron.comp.WishMessageGenerator;
 
 public class MainApp {
 	public static void main(String[] args) {
-		FileSystemResource fileSystemResource = new FileSystemResource("./src/main/resources/applicationContext.xml");
 	
-		System.out.println("*************BeanFactory container starting***************");
-		XmlBeanFactory xmlBeanFactory = new XmlBeanFactory(fileSystemResource);
-		System.out.println("*************BeanFactory container started************");
+		System.out.println("*************ClassPathXmlApplicationContext container starting***************");
+		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("/in/ineuron/cfg/applicationContext.xml");
 		
-		WishMessageGenerator wmg = xmlBeanFactory.getBean("wmg", WishMessageGenerator.class);
+		System.out.println("*************ClassPathXmlApplicationContext container started************");
+		
+		WishMessageGenerator wmg = classPathXmlApplicationContext.getBean("wmg", WishMessageGenerator.class);
 		System.out.println("HashCode of wmg is " + wmg.hashCode());
 		
 		String result = wmg.generateMessage("sachin");
 		System.out.println(result + "\n");
 		
-		WishMessageGenerator wmg1 = xmlBeanFactory.getBean("wmg", WishMessageGenerator.class);
+		WishMessageGenerator wmg1 = classPathXmlApplicationContext.getBean("wmg", WishMessageGenerator.class);
 		System.out.println("HashCode of wmg1 is " + wmg1.hashCode());
 		
 		String result1 = wmg1.generateMessage("Virat");
 		System.out.println(result1 + "\n");
 		
-		System.out.println("**********Container is closing***********");
+		System.out.println("**********ClassPathXmlApplicationContext is closing***********");
 	}
 }
